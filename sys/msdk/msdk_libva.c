b102f2970cc689d1e72bee768258a654c3db139f
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2017-12-14 17:04:20 +0000

    msdk : Add RenderNode support
    Use drm render node as the first choice of device node file.
    Fall backs to use drm primary (/dev/dri/card[0-9])
    if there is no render node available
    
    Basic logic is inherited from gstreamer-vaapi, but using
    gudev API rather than libudev directly.
    
    Added gudev library as dependency for msdk.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=791599

--- sys/msdk/msdk_libva.c
+++ sys/msdk/msdk_libva.c
@@ -39,6 +39,7 @@
 
 #include <va/va_drm.h>
 #include "msdk.h"
+#include <gudev/gudev.h>
 
 GST_DEBUG_CATEGORY_EXTERN (gst_msdkenc_debug);
 #define GST_CAT_DEFAULT gst_msdkenc_debug
@@ -50,6 +51,64 @@ struct _MsdkContext
   VADisplay dpy;
 };
 
+static gint
+get_device_id (void)
+{
+  GUdevClient *client = NULL;
+  GUdevEnumerator *e = NULL;
+  GList *devices, *l;
+  GUdevDevice *dev, *parent;
+  const gchar *devnode_path;
+  const gchar *devnode_files[2] = { "renderD[0-9]*", "card[0-9]*" };
+  int fd = -1, i;
+
+  client = g_udev_client_new (NULL);
+  if (!client)
+    goto done;
+
+  e = g_udev_enumerator_new (client);
+  if (!e)
+    goto done;
+
+  g_udev_enumerator_add_match_subsystem (e, "drm");
+
+  for (i = 0; i < 2; i++) {
+    g_udev_enumerator_add_match_name (e, devnode_files[i]);
+    devices = g_udev_enumerator_execute (e);
+
+    for (l = devices; l != NULL; l = l->next) {
+      dev = (GUdevDevice *) l->data;
+
+      parent = g_udev_device_get_parent (dev);
+      if (strcmp (g_udev_device_get_subsystem (parent), "pci") != 0) {
+        g_object_unref (parent);
+        continue;
+      }
+      g_object_unref (parent);
+
+      devnode_path = g_udev_device_get_device_file (dev);
+      fd = open (devnode_path, O_RDWR | O_CLOEXEC);
+      if (fd < 0)
+        continue;
+      GST_DEBUG ("Opened the drm device node %s", devnode_path);
+      break;
+    }
+
+    g_list_foreach (devices, (GFunc) gst_object_unref, NULL);
+    g_list_free (devices);
+    if (fd >= 0)
+      goto done;
+  }
+
+done:
+  if (e)
+    g_object_unref (e);
+  if (client)
+    g_object_unref (client);
+
+  return fd;
+}
+
 static gboolean
 msdk_use_vaapi_on_context (MsdkContext * context)
 {
@@ -58,12 +117,10 @@ msdk_use_vaapi_on_context (MsdkContext * context)
   VADisplay va_dpy = NULL;
   VAStatus va_status;
   mfxStatus status;
-  /* maybe /dev/dri/renderD128 */
-  static const gchar *dri_path = "/dev/dri/card0";
 
-  fd = open (dri_path, O_RDWR);
+  fd = get_device_id ();
   if (fd < 0) {
-    GST_ERROR ("Couldn't open %s", dri_path);
+    GST_ERROR ("Couldn't find a drm device node to open");
     return FALSE;
   }
 
