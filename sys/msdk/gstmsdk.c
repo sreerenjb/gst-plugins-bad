674524aa1ca637ca8463ce1c4f542416b06571e9
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2018-01-05 18:18:48 +0000

    msdk: Add mpeg2 decoder
    https://bugzilla.gnome.org/show_bug.cgi?id=792260

--- sys/msdk/gstmsdk.c
+++ sys/msdk/gstmsdk.c
@@ -41,6 +41,7 @@
 #include "gstmsdkh265enc.h"
 #include "gstmsdkmjpegdec.h"
 #include "gstmsdkmjpegenc.h"
+#include "gstmsdkmpeg2dec.h"
 #include "gstmsdkmpeg2enc.h"
 #include "gstmsdkvp8dec.h"
 #include "gstmsdkvp8enc.h"
@@ -54,6 +55,7 @@ GST_DEBUG_CATEGORY (gst_msdkh265enc_debug);
 GST_DEBUG_CATEGORY (gst_msdkmjpegdec_debug);
 GST_DEBUG_CATEGORY (gst_msdkmjpegenc_debug);
 GST_DEBUG_CATEGORY (gst_msdkmpeg2enc_debug);
+GST_DEBUG_CATEGORY (gst_msdkmpeg2dec_debug);
 GST_DEBUG_CATEGORY (gst_msdkvp8dec_debug);
 GST_DEBUG_CATEGORY (gst_msdkvp8enc_debug);
 
@@ -78,6 +80,8 @@ plugin_init (GstPlugin * plugin)
       "msdkmjpegenc");
   GST_DEBUG_CATEGORY_INIT (gst_msdkmpeg2enc_debug, "msdkmpeg2enc", 0,
       "msdkmpeg2enc");
+  GST_DEBUG_CATEGORY_INIT (gst_msdkmpeg2dec_debug, "msdkmpeg2dec", 0,
+      "msdkmpeg2dec");
   GST_DEBUG_CATEGORY_INIT (gst_msdkvp8dec_debug, "msdkvp8dec", 0, "msdkvp8dec");
   GST_DEBUG_CATEGORY_INIT (gst_msdkvp8enc_debug, "msdkvp8enc", 0, "msdkvp8enc");
 
@@ -102,6 +106,9 @@ plugin_init (GstPlugin * plugin)
 
   ret = gst_element_register (plugin, "msdkmjpegenc", GST_RANK_NONE,
       GST_TYPE_MSDKMJPEGENC);
+
+  ret = gst_element_register (plugin, "msdkmpeg2dec", GST_RANK_NONE,
+      GST_TYPE_MSDKMPEG2DEC);
 
   ret = gst_element_register (plugin, "msdkmpeg2enc", GST_RANK_NONE,
       GST_TYPE_MSDKMPEG2ENC);
